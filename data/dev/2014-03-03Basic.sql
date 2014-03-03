dd15ed6c44c468acefe00e89a4c31265305a1125
qiansen1386 <qiansen1386@gmail.com>
2014-03-03 15:39:15 +0800

    创建数据文件夹，添加MySQL workbench EER数据模型Create Data Folder Add entity relationship model
    Signed-off-by: qiansen1386 <qiansen1386@gmail.com>

---
+++ data/dev/2014-03-03Basic.sql
@@ -0,0 +1,120 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.2.2
+-- http://www.phpmyadmin.net
+--
+-- 主机: 127.0.0.1
+-- 生成日期: 2014 年 02 月 27 日 15:23
+-- 服务器版本: 5.5.27
+-- PHP 版本: 5.4.7
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- 数据库: `forum`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `tbl_node`
+--
+
+CREATE TABLE IF NOT EXISTS `tbl_node` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `summary` varchar(255) NOT NULL,
+  `topics_count` int(11) NOT NULL,
+  `lft` int(10) unsigned NOT NULL,
+  `rgt` int(10) unsigned NOT NULL,
+  `level` smallint(5) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `lft` (`lft`),
+  KEY `rgt` (`rgt`),
+  KEY `level` (`level`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+
+--
+-- 转存表中的数据 `tbl_node`
+--
+
+INSERT INTO `tbl_node` (`id`, `name`, `summary`, `topics_count`, `lft`, `rgt`, `level`) VALUES
+(1, '根节点', '全站根节点', 0, 1, 6, 1),
+(2, 'yii2', '', 0, 2, 2, 2),
+(3, 'gii', 'sss', 0, 5, 2, 4);
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `tbl_reply`
+--
+
+CREATE TABLE IF NOT EXISTS `tbl_reply` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `topic_id` int(11) NOT NULL,
+  `content` text NOT NULL,
+  `source` text NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `created_at` int(11) NOT NULL,
+  `updated_at` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `tbl_topic`
+--
+
+CREATE TABLE IF NOT EXISTS `tbl_topic` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `content` text NOT NULL,
+  `source` text NOT NULL,
+  `node_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `created_at` int(11) NOT NULL,
+  `updated_at` int(11) NOT NULL,
+  `replies_count` int(11) NOT NULL DEFAULT '0',
+  `last_reply_user_id` int(11) NOT NULL,
+  `replied_at` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `tbl_user`
+--
+
+CREATE TABLE IF NOT EXISTS `tbl_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `auth_key` varchar(32) NOT NULL,
+  `password_hash` varchar(255) NOT NULL,
+  `password_reset_token` varchar(32) DEFAULT NULL,
+  `email` varchar(255) NOT NULL,
+  `role` tinyint(4) NOT NULL DEFAULT '10',
+  `status` tinyint(4) NOT NULL DEFAULT '10',
+  `created_at` int(11) NOT NULL,
+  `updated_at` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+--
+-- 转存表中的数据 `tbl_user`
+--
+
+INSERT INTO `tbl_user` (`id`, `username`, `auth_key`, `password_hash`, `password_reset_token`, `email`, `role`, `status`, `created_at`, `updated_at`) VALUES
+(1, 'admin', 'qSKY2JHYGwo9TZk_mnmRWTGPJ3GsvXQU', '$2y$13$.V5qDRDpu.qB5gsjTgpdKOGBcK9yxev2ik.EQ6/GBRhkDRgLbrYje', NULL, 'admin@qq.com', 10, 10, 1388826199, 1388826199),
+(2, 'admin2', 'qSKY2JHYGwo9TZk_mnmRWTGPJ3GsvXQU', '$2y$13$.V5qDRDpu.qB5gsjTgpdKOGBcK9yxev2ik.EQ6/GBRhkDRgLbrYje', NULL, 'admin2@admin.com', 10, 10, 1393336571, 1393336571);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
