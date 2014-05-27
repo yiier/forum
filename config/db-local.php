8e1c4f67c6a96da8f1c3591d6aadc4881112953a
东方孤思子（Paris·QianSen） <qiansen1386@gmail.com>
2014-05-27 17:47:08 +0800

    创建 db-local 文件。方便分布式开发

---
+++ config/db-local.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+	'class' => 'yii\db\Connection',
+	'dsn' => 'mysql:host=127.0.0.1;dbname=forum',
+	'username' => 'root',
+	'password' => 'demo',
+	'charset' => 'utf8',
+	'tablePrefix' => 'tbl_',
+];
--- config/web.php
+++ config/web.php
@@ -1,7 +1,7 @@
 <?php
 
 $params = require(__DIR__ . '/params.php');
-$db = require(__DIR__ . '/db.php');
+$db = file_exists(__DIR__ . '/db-local.php') ? require(__DIR__ . '/db-local.php') : require(__DIR__ . '/db.php');
 
 $config = [
 	'id' => 'Yiier',
