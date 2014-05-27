ed77569f9eac4224b9341aa58bb4fe04bb839726
东方孤思子（Paris·QianSen） <qiansen1386@gmail.com>
2014-05-27 20:52:58 +0800

    调整MySQL时区

--- data/dev/2014-03-03Basic.sql
+++ data/dev/2014-03-03Basic.sql
@@ -8,7 +8,7 @@
 -- PHP 版本: 5.4.7
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+SET time_zone = "+08:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
